75092985cb31eb687f274a62e2a0a24b8ffeef09
Muttu Vittal <98959908+Muttu-Handigund@users.noreply.github.com>
2024-02-16 19:09:09 +0530

    First Program

---
+++ src/Day_01/FirstProgram.java
@@ -0,0 +1,13 @@
+package Day_01;
+
+public class FirstProgram {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("welcome!");
+		System.out.println("CSE!");
+		System.out.print("My Name Is Muttu!");
+	}
+
+
+}
