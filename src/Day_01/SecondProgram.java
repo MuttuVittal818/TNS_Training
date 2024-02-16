7f669e342e23048f5a2714708423c7f56d81fa2d
Muttu Vittal <98959908+Muttu-Handigund@users.noreply.github.com>
2024-02-16 19:45:20 +0530

    Second Program

---
+++ src/Day_01/SecondProgram.java
@@ -0,0 +1,18 @@
+package Day_01;
+
+public class SecondProgram {
+	public static void main(String[] args) {
+		
+		double firstNumber = 10d;
+		double secondNumber = 20d;
+		double result;
+		
+				result = firstNumber  / secondNumber;
+				System.out.println("result is " +result);
+				
+
+			}
+
+
+	}
+
