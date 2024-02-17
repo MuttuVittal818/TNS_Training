860c8962d7d3ec45d7ef6707e2c40f8e758c7603
Muttu Vittal <98959908+Muttu-Handigund@users.noreply.github.com>
2024-02-17 14:41:48 +0530

    Changes done

--- src/Day_02/BreakDemo.java
+++ src/Day_02/BreakDemo.java
@@ -4,8 +4,8 @@ public class BreakDemo {
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 
-		for (int i = 5; i < 10; i++) {
-			if (i == 5)
+		for (int i = 2; i < 10; i++) {
+			if (i == 2)
 				
 			System.out.println(i);
 			break;
